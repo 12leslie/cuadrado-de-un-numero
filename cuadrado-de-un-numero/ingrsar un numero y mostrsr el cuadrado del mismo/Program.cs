e02a795270925b0fd7bc8b49b79fa8537cae06bf
12leslie <106928251+12leslie@users.noreply.github.com>
2023-03-05 19:49:08 -0600

    Add files via upload
    0.1

---
+++ cuadrado-de-un-numero/ingrsar un numero y mostrsr el cuadrado del mismo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ejercicios // <--- en otros ejercicios me olvide de ponerlo, pero igualmente por el momento no son necesarios.
+{
+    class Ejercicio2
+    {
+        static void Main()
+        {
+            float num;
+
+            Console.Write("Ingrese un número:");
+            num = float.Parse(Console.ReadLine());
+
+            while (num < 1)
+            {
+                Console.Write("ERROR.Reingrese número:");
+                num = float.Parse(Console.ReadLine());
+            }
+
+            Console.Write("El cuadrado de {0} es {1}", num, num * num);
+            Console.Read();
+        }
+
+    }
+}
